223cdfe4519a8cfd607d47b3be46eb2ef409890b
maximzassdeveloper <fantom.zms14@gmail.com>
2022-09-10 12:20:59 +0300

    delete img animations

--- src/components/ui/CustomImage/CustomImage.tsx
+++ src/components/ui/CustomImage/CustomImage.tsx
@@ -16,7 +16,7 @@ export const CustomImage: FC<CustomImageProps> = ({
 
   const ref = useRef<HTMLDivElement>(null)
 
-  useAnimation(() => imageAnimation(ref))
+  // useAnimation(() => imageAnimation(ref))
 
   return (
     <div
